fe0beaaa3fd39757d989d1f94d35fbc8ff1f765d
uniQueSense <krzysztof.kurzak1@gmail.com>
2020-12-16 18:09:38 +0100

    ...

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -7,9 +7,10 @@ public class MyApp {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
-
-        System.out.println(messageService.getMessage());
+        MessageService messageService = applicationContext.getBean("messageService0", MessageService.class);
+        MessageService messageService1 = applicationContext.getBean("messageService", MessageService.class);
+        System.out.println(messageService.hashCode());
+        System.out.println(messageService1.hashCode());
 
         applicationContext.close();
     }
---
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Random;
+@Component("messageService0")
+
+public class RandomTextMessageService implements MessageService {
+
+    List<String> tablica = List.of("Wiadomosc: 1", "Wiadomosc: 2", "Wiadomosc: 3", "Wiadomosc: 4", "Wiadomosc: 5", "Wiadomosc: 6", "Wiadomosc: 7", "Wiadomosc: 8", "Wiadomosc: 9", "Wiadomosc: 10");
+
+
+    @Override
+    public String getMessage() {
+        int losowa_liczba = new Random().nextInt(9);
+        return tablica.get(losowa_liczba);
+    }
+}
