745a4a6ccd20268baa8a48c0ed1050a326e9f27e
uniQueSense <krzysztof.kurzak1@gmail.com>
2020-12-16 20:10:24 +0100

    ...

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -9,8 +9,15 @@ public class MyApp {
 
         MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
         MessageService messageService1 = applicationContext.getBean("messageService1", MessageService.class);
+        System.out.println("---------");
+        System.out.println(messageService.hashCode());
+        System.out.println(messageService1.hashCode());
+        System.out.println("---------");
         System.out.println(messageService.getMessage());
         System.out.println(messageService1.getMessage());
+        System.out.println("---------");
+
+
 
         applicationContext.close();
     }
